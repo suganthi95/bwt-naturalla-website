913657011658b76281bb9896d4aa0e2fcb38e0b8
bavabahurdeen <bahurdeen.works@blackwinstech.com>
2025-06-16 12:50:46 +0530

    product slug based get method added

--- src/common/FullScreenLoader.tsx
+++ src/common/FullScreenLoader.tsx
@@ -15,7 +15,7 @@ export default function FullScreenLoader() {
   </defs>
   <text fontSize="6" fill="black" textLength="220" lengthAdjust="spacingAndGlyphs">
     <textPath href="#circle" startOffset="0%">
-      Naturella • Naturella • Naturella • Naturella
+      Naturella • Naturella • Naturella • Naturella •
     </textPath>
   </text>
 </svg>
